954870b2fcb9a93c91a0019d1f62e592a020f0a6
Denys Kovalenko <denys.kovalenko@taxify.eu>
2018-11-25 08:26:12 +0200

    kinda created alert dialog

--- app/src/main/java/com/pavoldrotar/healthmo/EmergencyFragment.java
+++ app/src/main/java/com/pavoldrotar/healthmo/EmergencyFragment.java
@@ -1,6 +1,10 @@
 package com.pavoldrotar.healthmo;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -10,6 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class EmergencyFragment extends Fragment {
 
@@ -23,6 +31,47 @@ public class EmergencyFragment extends Fragment {
     public EmergencyFragment() {
 
     }
+
+    public void showAlertDialog(Integer bpm) {
+        AlertDialog dialog = new AlertDialog.Builder(this.getContext())
+                .setTitle("Do you feel ok?")
+                .setMessage("Shall we call your trusted contact?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO: Add positive button action code here
+                        Log.i("EmergencyFragment", "calling to trustee, bpm:" +  bpm);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null)
+                .create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            private static final int AUTO_DISMISS_MILLIS = 10000;
+            @Override
+            public void onShow(final DialogInterface dialog) {
+                final Button defaultButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                final CharSequence positiveButtonText = defaultButton.getText();
+                new CountDownTimer(AUTO_DISMISS_MILLIS, 100) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        defaultButton.setText(String.format(
+                                Locale.getDefault(), "%s (%d)",
+                                positiveButtonText,
+                                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) + 1 //add one so it never displays zero
+                        ));
+                    }
+                    @Override
+                    public void onFinish() {
+                        if (((AlertDialog) dialog).isShowing()) {
+                            dialog.dismiss();
+                        }
+                    }
+                }.start();
+            }
+        });
+        dialog.show();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +91,10 @@ public class EmergencyFragment extends Fragment {
                 i.putExtra("bpm", bpm);
                 i.putExtra("event", "bpm");
                 getContext().startService(i);
+
+                if (bpm > 150 || bpm < 40) {
+                    showAlertDialog(bpm);
+                }
             }
         };
         handler.postDelayed(r, 0);
